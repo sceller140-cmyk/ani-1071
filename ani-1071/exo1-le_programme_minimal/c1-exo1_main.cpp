5ca6395d991ebe85a6f326827f4aa0923a1d8a2a
sceller140-cmyk <sceller140@gmail.com>
2026-09-09 01:08:54 +0100

    Delete ani-1071 directory

--- ani-1071/exo1-le_programme_minimal/c1-exo1_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    // \n insère un saut de ligne directement dans la chaîne de caractères
-    std::cout << "TEFAK MBUNDA CLIVE BERNIE\n";
-    std::cout << "YAOUNDE\n";
-
-    return 0;
-}
