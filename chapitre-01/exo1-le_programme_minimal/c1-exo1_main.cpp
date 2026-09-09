060635a31180609029859cab3a5711730ba3deb5
sceller140-cmyk <sceller140@gmail.com>
2026-09-09 01:07:55 +0100

    Add files via upload

---
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // \n insère un saut de ligne directement dans la chaîne de caractères
+    std::cout << "TEFAK MBUNDA CLIVE BERNIE\n";
+    std::cout << "YAOUNDE\n";
+
+    return 0;
+}
